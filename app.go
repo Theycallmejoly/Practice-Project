55f13df1a1bebaba8fc1275b8af6b263e51fceeb
Yasin <144841869+Theycallmejoly@users.noreply.github.com>
2024-01-02 13:49:08 +0330

    Add files via upload

---
+++ app.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+
+	"example.com/practice/note"
+)
+
+func main() {
+	title, content := getNoteData()
+	userNote, err := note.New(title, content)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	userNote.Display()
+	err = userNote.Save()
+
+	if err != nil {
+		fmt.Println("Saving the note failed")
+		return
+	}
+
+	fmt.Println("saving the note succeeded!")
+
+}
+
+func getNoteData() (string, string) {
+	title := getUserData("Note title: ")
+	content := getUserData("Note content: ")
+	return title, content
+}
+
+func getUserData(prompt string) string {
+	fmt.Printf("%v", prompt)
+	reader := bufio.NewReader(os.Stdin)
+	text, err := reader.ReadString('\n')
+
+	if err != nil {
+		return ""
+	}
+
+	text = strings.TrimSuffix(text, "\n")
+	text = strings.TrimSuffix(text, "\r")
+
+	return text
+}
